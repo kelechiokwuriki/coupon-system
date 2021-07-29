a73432d4f363de02203912d95e499e7c80560088
Kelechi Okwuriki <kelechi@bento.africa>
2021-07-29 18:51:08 +0100

    items cards rendered

--- resources/js/components/Checkout.vue
+++ resources/js/components/Checkout.vue
@@ -13,10 +13,21 @@
                                         Items
                                     </div>
                                     <div class="card-body">
-
+                                        <div class="row" >
+                                            <div class="col-sm-6" v-for="item in items" v-bind:key="item.id">
+                                                <div class="card mb-4" style="width: 18rem;">
+                                                <div class="card-body">
+                                                    <h5 class="card-title">Card title</h5>
+                                                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                                    <a href="#" class="card-link">Card link</a>
+                                                    <a href="#" class="card-link">Another link</a>
+                                                </div>
+                                            </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-
                             </div>
                             <div class="col-sm-4">
                                  <div class="card">
@@ -56,6 +67,15 @@ export default {
     data() {
         return {
             items: []
+        }
+    },
+    mounted() {
+        this.getItems();
+    },
+    methods: {
+        async getItems() {
+            const items = await axios.get('/api/item');
+            this.items = items.data;
         }
     }
 }
