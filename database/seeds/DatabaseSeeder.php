681fe76aff87f0393289500d9e428ef122e44e26
Kelechi Okwuriki <kelechi@bento.africa>
2021-07-30 00:38:36 +0100

    database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Coupon;
+use App\Discount;
+use App\Rule;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +14,82 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $coupedFixedCreated = Coupon::create([
+            'code' => 'FIXED10',
+            'status' => 'available',
+        ]);
+
+        $couponPercent = Coupon::create([
+            'code' => 'PERCENT10',
+            'status' => 'available',
+        ]);
+
+        $couponMixed = Coupon::create([
+            'code' => 'PERCENT10',
+            'status' => 'available',
+        ]);
+
+        $couponRejected = Coupon::create([
+            'code' => 'REJECTED10',
+            'status' => 'available'
+        ]);
+
+
+        Rule::create([
+            'rule_type' => '>=',
+            'amount_limit' => '50',
+            'when_to_apply' => 'before',
+            'cart_amount_limit' => 1,
+            'coupon_id' => $coupedFixedCreated->id
+        ]);
+
+        Rule::create([
+            'rule_type' => '>=',
+            'amount_limit' => '100',
+            'when_to_apply' => 'before',
+            'cart_amount_limit' => 2,
+            'coupon_id' => $couponPercent->id
+        ]);
+
+        Rule::create([
+            'rule_type' => '>=',
+            'amount_limit' => '200',
+            'when_to_apply' => 'before',
+            'cart_amount_limit' => 3,
+            'coupon_id' => $couponMixed->id
+        ]);
+
+        Rule::create([
+            'rule_type' => '>=',
+            'amount_limit' => '1000',
+            'when_to_apply' => 'before',
+            'cart_amount_limit' => null,
+            'coupon_id' => $couponRejected->id
+        ]);
+
+        Discount::create([
+            'type' => 'mixed',
+            'discountAmount' => 10,
+            'coupon_id' => $coupedFixedCreated->id
+        ]);
+
+        Discount::create([
+            'type' => 'percent',
+            'discountAmount' => 10,
+            'coupon_id' => $couponPercent->id
+        ]);
+
+        Discount::create([
+            'type' => 'fixed',
+            'discountAmount' => 10,
+            'coupon_id' => $coupedFixedCreated->id
+        ]);
+
+        Discount::create([
+            'type' => 'rejected',
+            'discountAmount' => 10,
+            'coupon_id' => $couponRejected->id
+        ]);
+
     }
 }
