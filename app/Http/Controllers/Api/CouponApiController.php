bb39d7c82f88ed8ef2dad576fc8d4622a21bf3cc
Kelechi Okwuriki <kelechi@bento.africa>
2021-07-29 16:45:03 +0100

    error hadling in coupon controller

--- app/Http/Controllers/Api/CouponApiController.php
+++ app/Http/Controllers/Api/CouponApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Services\Coupon\CouponService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class CouponApiController extends Controller
 {
@@ -17,81 +18,12 @@ class CouponApiController extends Controller
 
     public function processCoupon(Request $request)
     {
-        return $this->couponService->processCoupon($request->all());
-    }
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        try {
+            $response = $this->couponService->processCoupon($request->all());
+            return response()->json($response, 200);
+        } catch (\Exception $e) {
+            Log::error('Unable to process coupon: ' . $e->getMessage());
+            return response()->json(['error' => 'Unable to process coupon'], 500);
+        }
     }
 }
